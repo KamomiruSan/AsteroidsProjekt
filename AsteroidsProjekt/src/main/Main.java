f2a84bdd25cd0f759bc51e3aa7b017f35d85446f
Anton Heřmanek <entonyworld@gmail.com>
2020-03-02 18:02:14 +0100

    Add files via upload

--- AsteroidsProjekt/src/main/Main.java
+++ AsteroidsProjekt/src/main/Main.java
@@ -1,9 +1,14 @@
-package main;
+package com.company.main;
 
-import com.company.obj.Display;
+        import com.company.game.AsteroidsGame;
+        import com.company.game.Display;
 
 public class Main {
-    public static void main (String[]args){
 
+    public static void main(String[] args) {
+        AsteroidsGame asteroidsGame = new AsteroidsGame();
+        Display display = new Display(asteroidsGame);
+        display.start();
     }
+
 }
